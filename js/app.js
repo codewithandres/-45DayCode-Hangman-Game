fe47ef9f6bca0270201911c5c01c5aa55870c203
Dize dev <axiozweb2022@gmail.com>
2024-05-15 23:13:48 -0500

    mostramos una preunta del juego de forma aleatoria y mos tramos los caracteres dependiedo del numero de letras de la palabra en custion

--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
+import { wordList } from "./game-word-list.js";
 
 const keywordDiv = document.querySelector('.keyboard');
+const wordDisplay = document.querySelector('.word-display');
+
+const getRandomWord = () => {
+    const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
+    document.querySelector('.hint-text b').textContent = hint;
+    wordDisplay.innerHTML = word.split('').map(() => `<li class="letter"></li>`).join('');
+};
 
 for (let i = 97; i <= 122; i++) {
     const buttons = document.createElement('button');
     buttons.textContent = String.fromCharCode(i);
     keywordDiv.appendChild(buttons);
 };
+
+getRandomWord();
